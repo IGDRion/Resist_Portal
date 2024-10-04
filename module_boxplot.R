7b2723c11b8c52dce874baf20543c7c73955ce1c
vlebars <victor.lebars@univ-rennes.fr>
2024-10-04 16:05:47 +0200

    Filter for count transcript boxplot (remove transcripts with values close to 0 tpm across all sample)

--- module_boxplot.R
+++ module_boxplot.R
@@ -4,17 +4,59 @@ boxplotUI <- function(id){
   
   # UI HERE
   column(width = 12, align = "center",
-         plotOutput(outputId = ns("countBoxplot"), height = "auto"))
+         uiOutput(outputId = ns("countBoxplot"))
+         )
   
 }
 
 
-# Server module example
+plot_count <- function(data, id) {
+  
+  if (!(sum(data$value >= 0.5))) {
+    
+    return(list(
+      type = "text",
+      content = paste0("No significant data for ", id)
+    ))
+    
+  } else {
+    
+    plot <- ggplot(data = data, aes(x = condition, y = value, fill = cancer_condition)) +
+      #geom_violin(trim = FALSE, alpha = 0.5) +
+      geom_boxplot(width = 0.2) +
+      geom_jitter(aes(shape = condition), size = 3) +
+      scale_y_continuous(labels = scales::comma) +
+      scale_fill_manual(values = c("Melanoma_Sensitive" = '#f5ab05' , "Melanoma_Resistant" = '#8c6100',
+                                   "Lung_Sensitive" = '#00cc94' , "Lung_Resistant" = '#00664a',
+                                   "Prostate_Sensitive" = '#0084cf' , "Prostate_Resistant" = '#014c75',
+                                   "Glioblastoma_Sensitive" = '#eb8dc1', "Glioblastoma_Resistant" = '#824a69'), 
+                        breaks=c("Melanoma_Sensitive" , "Melanoma_Resistant",
+                                 "Lung_Sensitive", "Lung_Resistant",
+                                 "Prostate_Sensitive", "Prostate_Resistant",
+                                 "Glioblastoma_Sensitive", "Glioblastoma_Resistant")) + 
+      labs(fill = "Cancer type", shape = "Condition") + 
+      guides(fill = guide_legend(override.aes = list(shape = NA))) + 
+      ylab("Normalized count (tpm)") +
+      xlab(" ") +
+      ggtitle(paste0("Count data of ", id, " across cancers")) +
+      theme_bw(base_size = 20) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      facet_wrap(~ cancer_type, scales = "fixed", ncol = 4)
+    
+    return(list(
+      type = "plot",
+      content = plot
+    ))
+    
+  }
+}
+
+# Server part
 boxplotServer <- function(id, type, count_boxplot_data, search_term) {
   moduleServer(
     id = id,
     module = function(input, output, session) {
-
+      
       # Reorder table for the plot
       count_boxplot_data$cancer_condition <- factor(count_boxplot_data$cancer_condition, 
                                                     levels = c("Melanoma_Sensitive", "Melanoma_Resistant",
@@ -22,58 +64,55 @@ boxplotServer <- function(id, type, count_boxplot_data, search_term) {
                                                                "Prostate_Sensitive", "Prostate_Resistant",
                                                                "Glioblastoma_Sensitive", "Glioblastoma_Resistant"))
       
-      plot_count <- function(data, id) {
-        
-        ggplot(data = data, aes(x = condition, y = value, fill = cancer_condition)) +
-          #geom_violin(trim = FALSE, alpha = 0.5) +
-          geom_boxplot(width = 0.2) +
-          geom_jitter(aes(shape = condition), size = 3) +
-          scale_y_continuous(labels = scales::comma) +
-          scale_fill_manual(values = c("Melanoma_Sensitive" = '#f5ab05' , "Melanoma_Resistant" = '#8c6100',
-                                       "Lung_Sensitive" = '#00cc94' , "Lung_Resistant" = '#00664a',
-                                       "Prostate_Sensitive" = '#0084cf' , "Prostate_Resistant" = '#014c75',
-                                       "Glioblastoma_Sensitive" = '#eb8dc1', "Glioblastoma_Resistant" = '#824a69'), 
-                            breaks=c("Melanoma_Sensitive" , "Melanoma_Resistant",
-                                     "Lung_Sensitive", "Lung_Resistant",
-                                     "Prostate_Sensitive", "Prostate_Resistant",
-                                     "Glioblastoma_Sensitive", "Glioblastoma_Resistant")) + 
-          labs(fill = "Cancer type", shape = "Condition") + 
-          guides(fill = guide_legend(override.aes = list(shape = NA))) + 
-          ylab("Normalized count (tpm)") +
-          xlab(" ") +
-          ggtitle(paste0("Count data of ", id, " across cancers")) +
-          theme_bw(base_size = 20) +
-          theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-          facet_wrap(~ cancer_type, scales = "fixed", ncol = 4)
-        
-      }
-      
-      # depending of the type variable when the module was called
-      # Do one plot for the whole gene
       if (type == "gene") {
+        boxplot_result <- plot_count(count_boxplot_data, search_term)
         
-        boxplot <- plot_count(count_boxplot_data, search_term)
+        output$countBoxplot <- renderUI({
+          if (boxplot_result$type == "plot") {
+            renderPlot({
+              boxplot_result$content
+            }, width = 1000, height = 500)
+          } else {
+            div(
+              style = "margin-top: 20px; font-size: 16px;",
+              p(boxplot_result$content)
+            )
+          }
+        })
         
-        output$countBoxplot <- renderPlot({
-          boxplot
-        }, width = 1000, height = 500)
-      
-      # Or one plot per transcript, on top of each other    
-      } else if (type == "tx"){
-        
-        boxplots <- count_boxplot_data %>%
+      } else if (type == "tx") {
+        boxplot_results <- count_boxplot_data %>%
           group_by(transcript_id) %>%
-          group_map(~ plot_count(.x, .y$transcript_id[1]))
+          group_split() %>%
+          map(~ plot_count(.x, .x$transcript_id[1]))
         
-        stacked_plot <- purrr::reduce(boxplots, `+`) + plot_layout(ncol = 1) # Stack all boxplot on top of each other
-        
-        output$countBoxplot <- renderPlot({
-          stacked_plot
-        }, width = 1000, height = function() {
-          500 * length(boxplots)  # Adjust height based on the number of plots
+        output$countBoxplot <- renderUI({
+          plot_outputs <- list()
+          text_outputs <- list()
+          
+          for (result in boxplot_results) {
+            if (result$type == "plot") {
+              plot_outputs[[length(plot_outputs) + 1]] <- result$content
+            } else {
+              text_outputs[[length(text_outputs) + 1]] <- result$content
+            }
+          }
+          
+          tagList(
+            if (length(plot_outputs) > 0) {
+              renderPlot({
+                wrap_plots(plot_outputs, ncol = 1)
+              }, width = 1000, height = 500 * length(plot_outputs))
+            },
+            if (length(text_outputs) > 0) {
+              div(
+                style = "margin-top: 20px; font-size: 16px;",
+                lapply(text_outputs, function(text) p(text))
+              )
+            }
+          )
         })
       }
-      
     }
   )
 }
